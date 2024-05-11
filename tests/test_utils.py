847b031841d3c650d94990d54b4e2dbac2cbd465
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-05-11 19:15:33 +0300

    Написать тест для функции executed_operations_only

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,19 @@
 from settings import TEST_OPERATIONS_PART
-from src.utils import read_file_data
+from src.utils import read_file_data, executed_operations_only
 
 
 def test_read_file_data():
     assert read_file_data(TEST_OPERATIONS_PART) == [{"id": 1}, {"id": 2}]
+
+
+def test_executed_operations_only():
+    my_list = [
+        {"id": 1, "state": "EXECUTED"},
+        {"id": 2, "state": "EXECUTED"},
+        {},
+        {"id": 3, "state": "dsf"}
+    ]
+    assert executed_operations_only(my_list) == [
+        {"id": 1, "state": "EXECUTED"},
+        {"id": 2, "state": "EXECUTED"},
+    ]
