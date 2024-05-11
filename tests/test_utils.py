8b01bc69b5b8a9d61c66b731d55483d81fdb0034
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-05-11 22:58:00 +0300

    Написать тесе для функции output_data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 from settings import TEST_OPERATIONS_PART
 from src.utils import read_file_data, executed_operations_only, translate_date_format, sort_operations_by_date, \
-    disguise_number
+    disguise_number, output_data
 
 
 def test_read_file_data():
@@ -48,3 +48,39 @@ def test_disguise_number():
     assert disguise_number("Счет 14211924144426031657") == "Счет **1657"
     assert disguise_number("Maestro 3928549031574026") == "Maestro 3928 54** **** 4026"
     assert disguise_number('') == ''
+
+
+def test_output_data():
+    test_dict1 = {
+        "id": 596171168,
+        "state": "EXECUTED",
+        "date": "2018-07-11T02:26:18.671407",
+        "operationAmount": {
+            "amount": "79931.03",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Открытие вклада",
+        "to": "Счет 72082042523231456215"
+    }
+    expected_output1 = '''\n11.07.2018 Открытие вклада\n -> Счет **6215\n79931.03 руб.\n'''
+    test_dict2 = {
+        "id": 716496732,
+        "state": "EXECUTED",
+        "date": "2018-04-04T17:33:34.701093",
+        "operationAmount": {
+            "amount": "40701.91",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Visa Gold 5999414228426353",
+        "to": "Счет 72731966109147704472"
+    }
+    expected_output2 = """\n04.04.2018 Перевод организации\nVisa Gold 5999 41** **** 6353 -> Счет **4472\n40701.91 USD\n"""
+    assert output_data(test_dict1) == expected_output1
+    assert output_data(test_dict2) == expected_output2
