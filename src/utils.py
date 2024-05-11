61358229478dd184c548ea4c1953fab8b6e9e58c
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-05-11 19:13:16 +0300

    Написать функцию создания списка с удачными операциями executed_operations_only

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,15 @@ def read_file_data(path: Path) -> list[dict]:
     """
     with open(path, 'r', encoding='utf-8') as file:
         return loads(file.read())
+
+
+def executed_operations_only(list_operations: list[dict]) -> list[dict]:
+    """
+    Функция определяет новый список со словарями,
+    в котором указаны только удачные операции
+    :param list_operations: список со словарями из файла operations.json
+    :return: список с удачными операциями
+    """
+    return [
+        op for op in list_operations if op.get('state') == "EXECUTED"
+    ]
